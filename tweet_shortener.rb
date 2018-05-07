02ff67b8d0d3d2426aa79108d7cf823a92d707c6
Steven Chen <ysteven.chen2@gmail.com>
2018-05-07 19:08:28 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,47 @@
 # Write your code here.
+def dictionary
+  substitute={
+    "hello" => "hi",
+    "to" => "2",
+    "two" =>"2",
+    "too" => "2",
+    "For" => "4",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+
+end
+
+def word_substituter(string)
+  string.split.collect do |x|
+    if dictionary.keys.include? x
+      dictionary[x]
+    else
+      x
+    end
+  end.join(" ")
+end
+
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |x|
+    puts word_substituter(x)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  selective_tweet_shortener(tweet)[0,140]
+
+end
