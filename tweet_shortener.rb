18bbb41845725fc381b309dc60b4d13a9ce5f502
Steven Chen <ysteven.chen2@gmail.com>
2018-05-07 18:57:08 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def dictionary
+  substitute={
+    "hello" => "hi",
+    "to" => "2",
+    "two" =>"2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"  
+  }
+  
+end
+
+def word_substituter(string)
+  string.split.collect do |x|
+    if dictionary.keys.include? x
+      dictionary[x]
+    else
+      x
+    end
+  end.join(" ")
+end
